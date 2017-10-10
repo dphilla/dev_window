e9ce7187f56be0df02ca9a8763a5b018a5c0ee17
Daniel Phillips <danieledward.phillips@gmail.com>
2017-10-10 15:52:46 -0600

    add view conditions for being logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
 
   def create
+
+    #require 'pry'; binding.pry
     if user = User.create_from_oauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
     end
     redirect_to root_path
+  end
+
+  def destroy
+   session[:user_id] = nil
+   redirect_to root_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
    def self.create_from_oauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                = auth_info.uid
-      new_user.name               = auth_info.name
-      new_user.username           = auth_info.username
+      new_user.name               = auth_info.info.name
+      new_user.username           = auth_info.nickname
       new_user.oauth_token        = auth_info.credentials.token
     end
   end
